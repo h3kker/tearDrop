6f3a1d0b998ed9ab48e1ee8d5858a11e19423180
Heinz Ekker <heinz.ekker@csf.ac.at>
2014-11-30 01:14:01 +0100

    msa viewer

--- lib/TearDrop/Task/MAFFT.pm
+++ lib/TearDrop/Task/MAFFT.pm
@@ -37,19 +37,38 @@ sub run {
   for my $l (split "\n", $out) {
     if ($l =~ m#^>\s*([^\s]+)\s*#) {
       $cur_id=$1;
-      $msa->{$cur_id}={
-        key => $cur_id,
-        last_pos => 0,
-        values => [],
-      };
+      $msa->{$cur_id}={id => $cur_id, seq => '', blocks => []};
     }
     elsif ($cur_id) {
-      for my $c (split '', $l) {
-        push @{$msa->{$cur_id}{values}}, [$msa->{$cur_id}{last_pos}++, $c eq '-' ? 0 : 1];
-      }
+      $msa->{$cur_id}{seq}.=$l;
     }
   }
-  $self->result([ sort { $a->{key} cmp $b->{key} } values %$msa ]);
+  for my $t (keys %$msa) {
+    my $cur=0;
+    my $cur_block;
+    my $match=0;
+    my $state='init';
+    for my $c (split '', $msa->{$t}{seq}) {
+      if ($c ne '-' && $state ne 'match') {
+        $state='match';
+        $cur_block=[$cur, 0];
+        $match++;
+      }
+      elsif ($c ne '-') {
+        $match++;
+        $cur_block->[1]=$cur;
+      }
+      elsif ($c eq '-' && $state ne 'gap') {
+        $state='gap';
+        push @{$msa->{$t}{blocks}}, $cur_block if $cur_block;
+      }
+      $cur++;
+    }
+    push @{$msa->{$t}{blocks}}, $cur_block if $cur_block;
+    $msa->{$t}{match}=$match;
+    $msa->{$t}{match_ratio}=$match/length($msa->{$t}{seq});
+  }
+  [ sort { $b->{match_ratio} <=> $a->{match_ratio} } values %$msa ];
 }
 
 1;
